d1d98f248208944a98ab6efeaa8933e2acc9452f
guozhuang <guo3525732@163.com>
2020-04-21 20:49:47 +0800

    struct

--- test/tree/tree.go
+++ test/tree/tree.go
@@ -2,17 +2,17 @@ package tree
 
 import "fmt"
 
-type TreeNode struct {
+type Node struct {
 	Value       int
-	Left, Right *TreeNode
+	Left, Right *Node
 }
 
-func (node *TreeNode) GetVal() {
+func (node *Node) GetVal() {
 	fmt.Println(node.Value)
 }
 
 //设计用来pipeline方式调用
-func (node *TreeNode) SetVal(val int) *TreeNode {
+func (node *Node) SetVal(val int) *Node {
 	node.Value = val
 	//因为返回来对应的实例来实现链式调用：node.SetVal(5).GetVal()//这样就实现包内方法的链式调用
 	return node
