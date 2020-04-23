3965bfd8e5b76f58963d100ab56bde49456faf9e
guozhuang <guo3525732@163.com>
2020-04-23 21:24:21 +0800

    interface

--- test/oop/entry/main.go
+++ test/oop/entry/main.go
@@ -26,12 +26,25 @@ func main() {
 		Contents: "this is mock result",
 	}
 
+	//对接口变量的类型%T可以看出其类型的不同，于是又引出了通过接口来实现多态【type】
 	fmt.Printf("%T, %v\n", r, r)
 
 	//real
-	r = &real.Retrieve{} //实际调用正常实现【所以golang中调用方决定被使用者实现了Retriever接口，于是接口变量的赋值来进行调用方】
-	fmt.Printf("%T, %v", r, r)
+	r = &real.Retrieve{
+		UserAgent: "ua",
+		TimeOut:   3,
+	} //实际调用正常实现【所以golang中调用方决定被使用者实现了Retriever接口，于是接口变量的赋值来进行调用方】
+	fmt.Printf("%T, %v\n", r, r)
 	//res := download(r)//此时需要被调用者的实现Get方法，通过接口变量的切换就能切换被调用者处理的流
 
 	//fmt.Println(res)
+
+	//接口变量的多态判断【因为存在接口变量被多个类型进行赋值，而需要对接口变量类型进行判断，便是多个实例的多态判断实现】
+	//所以使用接口来实现多态模式
+	switch v := r.(type) {
+	case *mock.Retrieve: //需要对应这里的指针
+		fmt.Println("mock", v.Contents)
+	case *real.Retrieve:
+		fmt.Println("real", v.UserAgent)
+	}
 }
